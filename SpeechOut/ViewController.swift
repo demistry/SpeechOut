0c3483c0685cdb5487c4cccb0ad334ac5e53b613
Demistry <dave11ilen@gmail.com>
2019-04-16 18:38:00 +0100

    Initial Commit

---
+++ SpeechOut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechOut
+//
+//  Created by David Ilenwabor on 16/04/2019.
+//  Copyright © 2019 Davidemi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
